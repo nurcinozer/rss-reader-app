d924552bbea5d7a553c8ed4fd5fdb2003f8b6979
Nurçin <nrcinozer@gmail.com>
2022-10-31 16:05:19 +0300

    added interceptor to app module

--- services/backend/src/app.module.ts
+++ services/backend/src/app.module.ts
@@ -2,12 +2,13 @@ import { CacheModule, Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { PrismaModule } from '@/prisma/prisma.module';
 import { AuthModule } from '@/auth/auth.module';
 import { UserModule } from '@/user/user.module';
-import { FeedModule } from './feed/feed.module';
+import { FeedModule } from '@/feed/feed.module';
 import * as redisStore from 'cache-manager-redis-store';
+import CustomHttpCacheInterceptor from '@/feed/interceptors/custom-http-cache.interceptor';
 
 @Module({
   imports: [
@@ -31,6 +32,10 @@ import * as redisStore from 'cache-manager-redis-store';
   ],
   controllers: [],
   providers: [
+    {
+      provide: APP_INTERCEPTOR, // https://docs.nestjs.com/interceptors
+      useClass: CustomHttpCacheInterceptor,
+    },
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
